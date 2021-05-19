380acb95ddf6e4a8a4a81b6b03a20dd4fe2306f8
Basel Abu Tarboush <babutarboush@premierlending.com>
2021-05-19 06:44:52 -0700

    iteration

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -9,9 +9,17 @@
  * getOdds([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [11, 35, 601, 777, 999]
  */
 function getOdds(numbers) {
-  // Your code here
+ 
+ let x =numbers.filter(function (num) {
+   return num %2 !== 0;
+ })
+ return x;
+ 
+
+
 }
 
+console.log(getOdds([1,2,9,6,3,,4,8]))
 /**
  * getEvens(numbers):
  * - receives an array of numbers called `numbers`
@@ -24,8 +32,13 @@ function getOdds(numbers) {
  */
 function getEvens(numbers) {
   // Your code here
+  let z = numbers.filter(function (num) {
+    return num %2 === 0;
+  })
+  return z;
 }
 
+console.log(getEvens([1,2,9,6,3,,4,8]))
 /**
  * countOccurences(x, numbers):
  * - receives a number `x`, and an array of numbers called `numbers`
@@ -35,10 +48,13 @@ function getEvens(numbers) {
  * countOccurences(1, [1, 2, 3, 1, 4, 5, 6, 1, 7, 8, 9, 10, 11, 1, 12, 13]) -> 4
  * countOccurences(52, [11, 35, 52, 14, 56, 601, 52, 777, 888, 999, 52]) -> 3
  */
-function countOccurences(x, numbers) {
-  // Your code here
+function countOccurences(p, numbers) {
+  let h = numbers.filter (function (number){
+    return number === p 
+  })
+  return h.length 
 }
-
+console.log (countOccurences(52, [11, 35, 52, 14, 56, 601, 52, 777, 888, 999, 52]))
 /**
  * makeThemDoctors(students):
  * - receives array `students`
@@ -49,7 +65,10 @@ function countOccurences(x, numbers) {
  */
 
 function makeThemDoctors(students) {
-  // Your code here
+ let dr = students.map(function(student){
+   return `Dr. ${student}` ;
+ });
+ return dr;
 }
-
+console.log (makeThemDoctors(['basel','ahmad','khaled']))
 module.exports = { getOdds, getEvens, countOccurences, makeThemDoctors };
